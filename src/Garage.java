1427bea123c6ac59f6c76f60c5ebbcfd92b650df
lilidemon <adlibitum7@vivaldi.net>
2018-08-12 17:30:13 +0200

    delete content of listeGarage.txt

--- src/Garage.java
+++ src/Garage.java
@@ -1,5 +1,4 @@
 
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -14,71 +13,71 @@ import Vehicules.Vehicule;
 
 public class Garage {
 
-		protected ArrayList<Vehicule> vehicules = new ArrayList<Vehicule>();
-		protected ArrayList<Option> options = new ArrayList<Option>();
-		
-		public Garage() {
-			readFile();
-		}
-		
-		
-		public String toString() {
-			
-			StringBuilder str = new StringBuilder();
-	        str.append(System.lineSeparator());
-
-	        for (Vehicule tuture : vehicules) {
-	            str.append(tuture.toString()).append(System.lineSeparator());
-	            
-	        }
-      
-	        return str.toString();
+	protected ArrayList<Vehicule> vehicules = new ArrayList<Vehicule>();
+	protected ArrayList<Option> options = new ArrayList<Option>();
 	
+
+	public Garage() {
+		readFile();
+	}
+
+	public String toString() {
+
+		StringBuilder str = new StringBuilder();
+		str.append(System.lineSeparator());
+
+		for (Vehicule tuture : vehicules) {
+			str.append(tuture.toString()).append(System.lineSeparator());
+
 		}
-		
-		
-		
-		public void readFile() {
-			
-////////////////READING ////////////////
+
+		return str.toString();
+
+	}
+
+	public void readFile() {
+
+		//////////////// READING ////////////////
 		File file = new File("listeGarage.txt");
 		BufferedReader bufferedReader = null;
 		String messageEmptyGarage = "Aucune voiture sauvegardée !\n";
-		
 
 		try {
 			FileReader fileReader = new FileReader(file);
 			bufferedReader = new BufferedReader(fileReader);
 			String line;
-			
-			
+
 			if ((line = bufferedReader.readLine()) != null) {
 				System.out.println("***************************");
-			   	System.out.println("*  Garage OpenClassrooms  *");
-			   	System.out.println("***************************");
-			   	
-			   	do {
-			   		System.out.println(line);
-			   	} while ((line = bufferedReader.readLine()) != null);
-			   	
+				System.out.println("*  Garage OpenClassrooms  *");
+				System.out.println("***************************");
+
+				do {
+					System.out.println(line);
+				} while ((line = bufferedReader.readLine()) != null);
+
 			} else {
+//				throw new EmptyGarageException(
+//						"Aucune voiture sauvegardée !\n");
 				System.err.println(messageEmptyGarage);
 				System.out.println("***************************");
-			   	System.out.println("*  Garage OpenClassrooms  *");
-			   	System.out.println("***************************");
+				System.out.println("*  Garage OpenClassrooms  *");
+				System.out.println("***************************");
+				
+				
+				
 			}
 
-		
 		} catch (FileNotFoundException e) {
 			System.err.printf("Le fichier %s n'a pas été trouvé.", file.toString());
 		} catch (IOException e) {
 			System.err.println("Impossible de lire le contenu du fichier " + file.toString());
 		} finally {
 			// execute finally quoiqu'il arrive
-			
+
 			try {
-				
-				if(bufferedReader != null) {
+
+				if (bufferedReader != null) {
 					bufferedReader.close();
 				}
 			} catch (IOException e) {
@@ -88,34 +87,25 @@ public class Garage {
 			}
 
 		}
+	}
 
-		}
-		
+	public void addVoiture(Vehicule vehicule) {
+		this.vehicules.add(vehicule);
+		File file = new File("listeGarage.txt");
 
-		public void addVoiture(Vehicule vehicule) {
-			this.vehicules.add(vehicule);
-			File file = new File("listeGarage.txt");
-	
-			
-			
-			
-//////////////// WRITTING ////////////////
+		//////////////// WRITTING ////////////////
 
-			try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
-				
-				for (Vehicule tuture : vehicules) {
-		            bufferedWriter.write(tuture.toString());
-		            bufferedWriter.newLine();
-		        }
-	
-			} catch (IOException e) {
-				e.printStackTrace();
+		try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+
+			for (Vehicule tuture : vehicules) {
+				bufferedWriter.write(tuture.toString());
+				bufferedWriter.newLine();
 			}
-		
-			
+
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 
-
-
+	}
 
 }
